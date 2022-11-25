43e59583fbad5c8ac4532d11c65e07914cefd93c
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2022-11-24 23:33:59 -0300

    Feat: Fix mysql conn and add user manualy to bd

--- src/main/java/com/userRegistration/Modules/User/controller/CreateUserController.java
+++ src/main/java/com/userRegistration/Modules/User/controller/CreateUserController.java
@@ -1,5 +1,7 @@
 package com.userRegistration.Modules.User.controller;
 
+import com.userRegistration.Modules.User.dao.UserDAO;
+import com.userRegistration.Modules.User.model.UserModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +12,9 @@ public class CreateUserController {
     private static final String template = "Hello, %s!";
 
     @GetMapping("/sla")
-    public String list(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format(template, name);
+    public Boolean list(@RequestParam(value = "name", defaultValue = "World") String name) {
+        UserModel user = new UserModel("Paulo Eduardo de Sordi Gomes", "paulo@email.com", "Paulo123");
+        Boolean result = UserDAO.createUser(user);
+        return result;
     }
 }
--- src/main/java/com/userRegistration/Shared/Mysql/MySQL.java
+++ src/main/java/com/userRegistration/Shared/Mysql/MySQL.java
@@ -5,21 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySQL {
-    private static Connection connection = null;
-
     public static Connection getConnection() {
-        if (connection == null) {
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                String url = System.getenv("MYSQL_URL");
-                if (url == null) {
-                    throw new RuntimeException("MYSQL_URL environment variable not set");
-                }
-                connection = DriverManager.getConnection(url);
-            } catch (ClassNotFoundException | SQLException e) {
-                e.printStackTrace();
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = System.getenv("MYSQL_URL");
+            if (url == null) {
+                throw new RuntimeException("MYSQL_URL environment variable not set");
             }
+            conn = DriverManager.getConnection(url);
+            if (conn == null) {
+                throw new RuntimeException("Failed to connect to database");
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
-        return connection;
+        return conn;
     }
 }
